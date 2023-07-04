cc2f515fced8424c8ae344dfdebdd8a240cf9b6b
jke94 <jcarracedo.dev@gmail.com>
2023-07-04 19:09:11 +0200

    Basic health check implementation for the connection with the database.

---
+++ SoccerCrud.WebApi/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,40 @@
+﻿namespace SoccerCrud.WebApi.Health
+{
+    #region using
+
+    using Microsoft.Extensions.Diagnostics.HealthChecks;
+    using SoccerCrud.WebApi.Repositories;
+
+    #endregion
+
+    public class DatabaseHealthCheck : IHealthCheck
+    {
+        #region Fields
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DatabaseHealthCheck(
+            IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #endregion
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context, 
+            CancellationToken cancellationToken = new ())
+        {
+            try
+            {
+                var player = await _unitOfWork.PlayerRepository.GetAsyncById(new Guid());
+
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception exception)
+            {
+                return HealthCheckResult.Unhealthy(exception: exception);
+            }
+        }
+    }
+}
